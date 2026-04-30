8e73c401d6680a0d65b75ec8234bbb5383ea963b
KuniokiSaito <kuniokisaito@KuniokiSaitonoMacBook-Air.local>
2026-04-30 20:13:50 +0900

    Project4：Mult.asmを作成した

--- projects/4/mult/Mult.asm
+++ projects/4/mult/Mult.asm
@@ -7,4 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// R2とnを0に設定
+    @R2
+    M=0
+    @n
+    M=0
+(LOOP)
+    // if(n == R1) goto END
+    @n
+    D=M
+    @R1
+    D=D-M
+    @END
+    D;JEQ
+    // R2 = R2 + R0
+    @R2
+    D=M
+    @R0
+    D=D+M
+    @R2
+    M=D
+    // n = n + 1
+    @n
+    M=M+1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP   
